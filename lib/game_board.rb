d93d6ac8c6c569137cea66f1d7ee468474becf15
desanchezh <129230744+desanchezh@users.noreply.github.com>
2024-11-08 22:25:14 -0500

    You can now print colors

--- lib/game_board.rb
+++ lib/game_board.rb
@@ -1,2 +1,12 @@
 class GameBoard
+  def initialize
+    @game_board = Array.new(4)
+    @possible_colors = {
+      blue: 'blue'.colorize(:blue),
+      red: 'red'.colorize(:red),
+      green: 'green'.colorize(:green)
+    }
+  end
+
+  attr_reader :possible_colors
 end
--- main.rb
+++ main.rb
@@ -1,3 +1,8 @@
 require_relative 'lib/computer'
 require_relative 'lib/game_board'
 require_relative 'lib/player'
+require 'colorize'
+
+board = GameBoard.new
+
+puts board.possible_colors.values
